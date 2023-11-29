01c7b0b2a492078599d948766f23740efb4d3637
LBK-miq <2049932883@qq.com>
2023-11-29 14:58:44 +0800

    help page

---
+++ src/components/Help.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="ui container">
+      <div class="ui vertical masthead segment">
+        <h1 class="ui header">
+          简要说明
+        </h1>
+
+        <div class="ui segment">
+          <h3 class="ui header">还有其他问题吗？</h3>
+          <p>对于尚未提及的问题，可以联系制作组寻求帮助（见“关于”-“制作者名单 & 联系我们”部分）。</p>
+          <p>To be continued...</p>
+        </div>
+
+      </div>
+    </div>
+  </template>
---
+++ src/views/Help.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navigator from '../components/Navigator.vue'
+import About from "../components/Help.vue"
+</script>
+
+<template>
+  <div id="root">
+    <header>
+      <Navigator activeItem="帮助" />
+    </header>
+
+    <main>
+      <About />
+    </main>
+  </div>
+</template>
